1b567bc3e6b8e3622d59b62e4733cb9b355aec8d
Vimal999 <raviravi470471@gmail.com>
2026-07-25 16:34:14 +0530

    Day 6: JWT authentication and login

--- backend/api/users.py
+++ backend/api/users.py
@@ -3,8 +3,12 @@ from sqlalchemy.orm import Session
 
 from backend.database.database import get_db
 from backend.database.models import User
+
 from backend.schemas.user import UserCreate, UserResponse
-from backend.utils.security import hash_password
+from backend.schemas.login import LoginRequest, Token
+
+from backend.utils.security import hash_password, verify_password
+from backend.utils.auth import create_access_token
 
 router = APIRouter(
     prefix="/users",
@@ -47,3 +51,35 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
 
     return user
+
+
+@router.post("/login", response_model=Token)
+def login(user: LoginRequest, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        user.password,
+        existing_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        {"sub": existing_user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
